2cd9a9d00e799f2d2c2c09931859df7b7bfbbdc7
leesang825 <leesang825@gmail.com>
2026-08-20 15:50:05 +0900

    Add files via upload

---
+++ Day01/Swap.java
@@ -0,0 +1,15 @@
+package start;
+
+public class Swap {
+
+	public static void main(String[] args) {
+		
+		int a = 10 , b = 25, swap = 15;
+		
+		
+		System.out.println("Swapping의 결과> 변수 a의 값 =" +(a+swap));
+		System.out.println("Swapping의 결과> 변수 b의 값 =" +(b-swap));
+		
+	}
+
+}
